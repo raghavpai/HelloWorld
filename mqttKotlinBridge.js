bf688d12077986819dd6eea31cc90c8d60c41bd1
Raghavendra Pai <raghavendpai@gmail.com>
2025-02-25 10:25:56 +0530

    update
    update

--- mqttKotlinBridge.js
+++ mqttKotlinBridge.js
@@ -25,3 +25,28 @@ function subscribe(topic) {
 function unsubscribe(topic) {
     window.mqttClient.unsubscribe(topic);
 }
+
+function createMQTTClient() {
+    const hostname = window.composeApp.com.gsb.vyapar.bhagya.mqtt.getMqttUrl();
+    const port = window.composeApp.com.gsb.vyapar.bhagya.mqtt.getMqttPort();
+    const clientId = "clientId_" + Math.random().toString(36).substring(2, 15);
+    const client = new Paho.MQTT.Client(hostname, port, clientId);
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+    window.mqttClient = client
+}
+
+function onConnect() {
+  console.log("onConnect");
+  window.composeApp.com.gsb.vyapar.bhagya.mqtt.connectComplete();
+}
+
+function onConnectionLost(responseObject) {
+  console.log("onConnectionLost: " + responseObject.errorMessage);
+  window.composeApp.com.gsb.vyapar.bhagya.mqtt.connectionLost(responseObject.errorMessage)
+}
+
+function onMessageArrived(message) {
+  console.log("onMessageArrived: " + message.payloadString);
+  window.composeApp.com.gsb.vyapar.bhagya.mqtt.messageArrived(message.destinationName, message.payloadString)
+}
